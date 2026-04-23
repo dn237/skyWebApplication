62196379dc0d7059353390edb26746c6212920ca
budaianita8-dev <budai.anita8@gmail.com>
2026-04-23 18:45:16 +0100

    restore models files

--- organizations/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Department
-
-
-@admin.register(Department)
-class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'dept_name', 'manager')
-    search_fields = ('dept_name',)
