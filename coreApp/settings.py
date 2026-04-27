22f52a2b9c10c01b644659070bc252c244493849
Diana Nichvolodova <w2016501@westminster.ac.uk>
2026-04-27 19:50:21 +0100

    .env setup

--- coreApp/settings.py
+++ coreApp/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,11 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%tul)q$e#7hg)scyq2**ums^-#4zb5pplrx3pv2(@kky%ff1k1"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Get the key from the environment
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
