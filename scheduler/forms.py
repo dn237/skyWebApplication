d29b822c46a7b33d772df9b3d1a1dcd37149045b
samiaahr <samia.elhayari@gmail.com>
2026-04-28 22:08:40 +0100

    Improve scheduler calendar UI and navigation

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -5,3 +5,26 @@ class ScheduleForm(forms.ModelForm):
     class Meta:
         model = Schedule
         fields = ['title', 'message', 'date', 'time', 'platform', 'schedule_type']
+        
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'custom-input',
+                'placeholder': 'Enter title'
+            }),
+            'message': forms.Textarea(attrs={
+                'class': 'custom-input',
+                'placeholder': 'Add description...'
+            }),
+            'platform': forms.TextInput(attrs={
+                'class': 'custom-input',
+                'placeholder': 'Zoom / Teams / etc'
+            }),
+            'date': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'custom-input'
+            }),
+            'time': forms.TimeInput(attrs={
+                'type': 'time',
+                'class': 'custom-input'
+            }),
+}
